e4ecfdae6216c787edd5594cf04510b10f1338fa
luweiqianyi <runningriven@163.com>
2024-02-16 21:18:17 +0800

    为项目增加googletest单元测试

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 #include <cstdio>
 #include <curl/curl.h>
+#include <gtest/gtest.h>
 
-int main()
+// https://everything.curl.dev/examples
+
+void SimpleHttpRequest()
 {
-    printf("%s\n","Hello world!");
-    return 0;
+    printf("Hello world\n");
+    CURL *curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://www.baidu.com/");
+
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+
+        curl_easy_cleanup(curl);
+    }
+}
+
+TEST(CURL, SimpleHttpRequest)
+{
+    SimpleHttpRequest();
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
